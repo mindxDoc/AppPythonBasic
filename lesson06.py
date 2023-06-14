fb44c8c7d255bdee232a2c3e01ab2ba6a2dc5d19
V.T.TuongDi <vothituongduy20@gmail.com>
2023-06-14 20:44:02 +0700

    update lesson 6

--- lesson06.py
+++ lesson06.py
@@ -1,7 +1,7 @@
 print("Bảng cửu chương")
 
 for i in range(2, 10):
-    for j in range(1, 10):
+    for j in range(1, 11):
         result = i * j
         print(i, " * ", j, " = ", result)
-    print()  # Add a blank line after each row
+    print() # Add a blank line after each row
